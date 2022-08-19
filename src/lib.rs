e51e83eb059a475bdadc8bfafb0feda06a226fa2
Reinis Muiznieks <muiznieks.reinis@gmail.com>
2022-08-19 15:55:29 +0300

    Move to 0.2 support only for now

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,82 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-#[cfg(feature = "hal_1_0")]
-pub mod as3910;
-#[cfg(feature = "hal_0_2")]
-pub mod as3910_2_7;
+use embedded_hal as hal;
+use hal::blocking::spi;
+use hal::digital::v2::OutputPin;
+
 pub mod command;
 pub mod register;
+
+use crate::register::Register;
+
+pub struct AS3910<SPI, CS> {
+    spi: SPI,
+    cs: CS,
+}
+
+impl<E, CS, SPI> AS3910<SPI, CS>
+where
+    SPI: spi::Transfer<u8, Error = E> + spi::Write<u8, Error = E>,
+    CS: OutputPin,
+{
+    pub fn new(spi: SPI, cs: CS) -> Self {
+        Self { spi, cs }
+    }
+
+    pub fn read(&mut self, reg: Register) -> Result<u8, E> {
+        let mut buffer = [reg.read_address(), 0];
+
+        self.with_cs_high(|mfr| {
+            let buffer = mfr.spi.transfer(&mut buffer)?;
+
+            Ok(buffer[1])
+        })
+    }
+
+    fn read_many<'b>(&mut self, reg: Register, buffer: &'b mut [u8]) -> Result<&'b [u8], E> {
+        let byte = reg.read_address();
+
+        self.with_cs_high(move |mfr| {
+            mfr.spi.transfer(&mut [byte])?;
+
+            let n = buffer.len();
+            for slot in &mut buffer[..n - 1] {
+                *slot = mfr.spi.transfer(&mut [byte])?[0];
+            }
+
+            buffer[n - 1] = mfr.spi.transfer(&mut [0])?[0];
+
+            Ok(&*buffer)
+        })
+    }
+
+    pub fn write(&mut self, reg: Register, val: u8) -> Result<(), E> {
+        self.with_cs_high(|mfr| mfr.spi.write(&[reg.write_address(), val]))
+    }
+
+    fn write_many(&mut self, reg: Register, bytes: &[u8]) -> Result<(), E> {
+        self.with_cs_high(|mfr| {
+            mfr.spi.write(&[reg.write_address()])?;
+            mfr.spi.write(bytes)?;
+
+            Ok(())
+        })
+    }
+
+    fn with_cs_high<F, T>(&mut self, f: F) -> T
+    where
+        F: FnOnce(&mut Self) -> T,
+    {
+        self.cs.set_high();
+        let result = f(self);
+        self.cs.set_low();
+
+        result
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum MyError<SPI> {
+    Spi(SPI),
+    // Add other errors for your driver here.
+}
